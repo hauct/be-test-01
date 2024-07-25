a3a3784863850392d4082d5a658d5715dee7d46c
hauct <cungtronghau@gmail.com>
2024-07-26 01:41:17 +0700

    edit

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!   ddddd')
 })
 
 app.get('/abc', (req, res) => {

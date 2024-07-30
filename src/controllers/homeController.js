d16b0707ffdedace848c8e4be36540156043d2d7
hauct <cungtronghau@gmail.com>
2024-07-31 01:26:10 +0700

    edit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const connection = require('../config/database')
+const {getAllUsers} = require('../services/CRUDService')
 
-const getHomepage = (req, res) => {
-    return res.render('home.ejs')
+const getHomepage = async (req, res) => {
+    let results = await getAllUsers()
+    return res.render('home.ejs', {listUsers: results})
 }
 
 const getABC = (req, res) => {
@@ -12,17 +14,13 @@ const getHoiDanIT = (req, res) => {
     res.render('sample.ejs')
 }
 
-const getCreatePage = (req, res) => {
-    res.render('create.ejs')
-}
-
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
     console.log(">>> req.body: ", req.body)
     let email = req.body.email;
     let name = req.body.name;
     let city = req.body.city;
     // let {email, name, city} = req.body
-    console.log(">>> email = ", email, " name = ", name, " city = ", city)
+    console.log(">>> email = ", email, " name = ", name, " city = ", city);
 
     // connection.query(
     //     `INSERT INTO Users (email, name, city) 
@@ -33,7 +31,19 @@ const postCreateUser = (req, res) => {
     //         res.send('Created user succesfully')
     //      }
     // )
+
+    let [results, fields] = await connection.query (
+        `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`, [email, name, city]
+    );
+
+    console.log(">>> check results :", results)
+    res.send('Created user succeed !')
 }
+
+const getCreatePage = (req, res) => {
+    res.render('create.ejs')
+}
+
 
 module.exports = {
     getHomepage,

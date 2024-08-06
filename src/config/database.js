c1eaa1dc19ec42611bc2b314dc7a7c4cec6ca268
hauct <cungtronghau@gmail.com>
2024-08-07 01:48:15 +0700

    edit

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,22 @@
 require('dotenv').config();
-const mysql = require('mysql2/promise');
+const mongoose = require('mongoose');
+
+const dbState = [{
+  value: 0,
+  label: "disconnected"
+},
+{
+  value: 1,
+  label: "connected"
+},
+{
+  value: 2,
+  label: "connecting"
+},
+{
+  value: 3,
+  label: "disconnecting"
+}];
 
 // create the connection
 // const connection = mysql.createConnection({
@@ -10,14 +27,14 @@ const mysql = require('mysql2/promise');
 //     password: process.env.DB_PASSWORD
 //   });
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit:0
-  });
+const connection = async () => { 
+  const options = {
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD,
+      dbName: process.env.DB_NAME
+  }
+  await mongoose.connect(process.env.DB_HOST, options);
+  const state = Number(mongoose.connection.readyState);
+  console.log(dbState.find(f => f.value == state).label, "to db");
+};
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express'); //commonjs
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 const connection = require('./config/database')
+const mongoose = require('mongoose')
 
 const app = express(); //app express
 const port = process.env.PORT || 8888;// port 
@@ -18,6 +19,27 @@ app.use(express.urlencoded({ extended:true }));
 // Khai báo routes
 app.use(webRoutes);
 
-app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const cat = new Kitten({ name: 'Meo meo' });
+
+console.log(cat.name); // 'Silence'
+
+cat.save();
+
+(async () => {
+  try {
+    await connection();
+    app.listen(port, hostname, () => {
+      console.log(`Backend zero app listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(`>>> Error connect to DB: `, error)
+  }
+}
+)()
+
